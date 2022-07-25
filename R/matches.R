a2d893efd9488fbc6aed7cdfe2f5ba2077f8a38b
Zach Timpe <zacharytimpe@gmail.com>
2022-07-25 08:48:06 -0700

    add documentation, manuals

--- R/matches.R
+++ R/matches.R
@@ -1,3 +1,17 @@
+#' Make a standard league-play tibble
+#'
+#' Given a league-play data frame from {engsoccer}, returns a tibble with
+#' standardised column-names and types, e.g. `date` is a `Date`.
+#'
+#' @param data_engsoc  obtained from {engsoccerdata}.
+#' @param country `character` scalar, specifies the league.
+#'
+#' @return a tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#'    `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#' @examples
+#' uss_make_matches(engsoccerdata::spain, "Spain")
 uss_make_matches <- function(data_engsoc,country) {
   result <-
     data_engsoc |>
