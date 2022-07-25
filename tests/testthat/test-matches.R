e165208c1e37d6d74f3e25bdff88ceed76b5f7f8
Zach Timpe <zacharytimpe@gmail.com>
2022-07-25 11:50:52 -0700

    add testing

---
+++ tests/testthat/test-matches.R
@@ -0,0 +1,15 @@
+test_that("uss_make_matches works", {
+  df <- uss_make_matches(engsoccerdata::italy,"Italy")
+
+  expect_true(tibble::is_tibble(df))
+
+  expect_named(
+  df, c("tier","season","date","home","visitor","goals_home","goals_visitor","country")
+               )
+
+  expect_identical(unique(df$country),"Italy")
+
+  expect_s3_class(df$tier,"factor")
+})
+
+
